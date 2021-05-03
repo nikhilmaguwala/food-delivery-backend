5102aca76b41d7e76d89c475f0d5d74ead1078b6
Nikhil Maguwala <nikhilmaguwala@gmail.com>
2021-05-03 16:49:38 +0530

    added SSl for host

--- models/index.js
+++ models/index.js
@@ -4,6 +4,9 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     dialect: dbConfig.dialect,
+    dialectOptions : {
+        ssl: true
+    },
     operatorsAliases: false,
     logging: false,
     pool: {
