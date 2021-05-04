14f0eb258f3bc1eedaef7aae21f819a37c1665ac
Nikhil Maguwala <nikhilmaguwala@gmail.com>
2021-05-04 13:13:42 +0530

    setup for seed and reset

--- server.js
+++ server.js
@@ -19,9 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./models");
-db.sequelize.sync({ force: true }).then(() => {
-    console.log('Dropped DB and Re-sync It')
-})
+db.sequelize.sync();
 
 // simple route
 app.get("/", (req, res) => {
